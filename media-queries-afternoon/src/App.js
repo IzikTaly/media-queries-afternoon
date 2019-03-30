6796f68d92071aa9429c3c4ce1405b8559b637e5
IzikTaly <izik.taly@gmail.com>
2019-03-30 10:00:54 -0600

    Add files via upload

---
+++ media-queries-afternoon/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Card from './Card';
+import './App.css';
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        
+    }
+
+
+    render() {
+
+        return(
+            <div className='app'>
+                <h1 class='head'>Coolhaver</h1>
+            </div> 
+        )
+    }
+}
